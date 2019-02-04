ee0b5b9f2ba3ca735ec18e9cf91a4517d643973b
Sme <esahig15789@gmail.com>
2019-02-04 14:04:19 -0500

    charlie work 1

--- MAKE HACK_quick.cmd
+++ MAKE HACK_quick.cmd
@@ -1,9 +1,9 @@
 cd %~dp0
 
-copy FE8_clean.gba SkillsTest.gba
+copy FE8_clean.gba FE-Fallen-Flame.gba
 
 cd "%~dp0Event Assembler"
 
-Core A FE8 "-output:%~dp0SkillsTest.gba" "-input:%~dp0ROM Buildfile.event"
+Core A FE8 "-output:%~dp0FE-Fallen-Flame.gba" "-input:%~dp0ROM Buildfile.event"
 
 pause
