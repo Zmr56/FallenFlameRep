1af80efb55aaad8c68f90849ec74106573c7bb84
Sme <esahig15789@gmail.com>
2020-05-17 17:05:46 -0400

    functional ace skills

--- MAKE HACK_debug.cmd
+++ MAKE HACK_debug.cmd
@@ -20,7 +20,10 @@ echo: | (pal2ea "%~dp0Battle_Palettes/Battle Palettes.txt")
 
 cd "%~dp0Event Assembler"
 
-ColorzCore A FE8 "-output:%~dp0FE-Fallen-Flame.gba" "-input:%~dp0Debug.event" --build-times
+ColorzCore A FE8 "-output:%~dp0FE-Fallen-Flame.gba" "-input:%~dp0Debug.event" --build-times --nocash-sym
+
+cd "%~dp0sym"
+java -jar %~dp0sym\SymCombo.jar "%~dp0FE-Fallen-Flame.sym" "%~dp0FE8_clean.sym"
 
 if exist "%~dp0ups/ups.exe" (
     cd "%~dp0ups"
